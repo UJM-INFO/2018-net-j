8c9cc554cf857bf065e3b315e9d8c77635471076
Aboud Zakaria <aboud.zakaria@gmail.com>
2018-03-17 13:23:13 +0100

    "enhance" desing

--- MemberUI.py
+++ MemberUI.py
@@ -19,9 +19,6 @@ txt_port = tk.Text(frm1, width=10, height=2); txt_port.pack(side=tk.LEFT)
 txt_port.delete(1.0, tk.END)
 txt_port.insert(tk.END, "1112")
 
-txt_data = tk.Text(frm1, width=10, height=2); txt_data.pack(side=tk.LEFT)
-txt_data.delete(1.0, tk.END)
-
 def click_run():
     p = txt_port.get(1.0, tk.END)
     global mem
@@ -37,21 +34,27 @@ def click_stop():
 
 btn_run = tk.Button(frm1, text="Stop Loops", command=click_stop); btn_run.pack(side=tk.LEFT)
 
+frm2 = tk.Frame(root)
+
+txt_data = tk.Text(frm2, width=20, height=2); txt_data.pack(side=tk.LEFT)
+txt_data.delete(1.0, tk.END)
+
 
 def click_create():
     blk = mem.blockChain.createBlock(txt_data.get(1.0, tk.END))
     if blk!=-1:
         mem.broadcastBlock(blk.id)
 
-btn_create = tk.Button(frm1, text="Create Block", command=click_create); btn_create.pack(side=tk.LEFT)
+btn_create = tk.Button(frm2, text="Create Block", command=click_create); btn_create.pack(side=tk.LEFT)
 
 def click_print():
     print(mem.blockChain)
 
-btn_print = tk.Button(frm1, text="Print Chain", command=click_print); btn_print.pack(side=tk.LEFT)
+btn_print = tk.Button(frm2, text="Print Chain", command=click_print); btn_print.pack(side=tk.LEFT)
 
 
 frm1.pack(side=tk.TOP)
+frm2.pack(side=tk.TOP)
 
 txt_log = tk.Text(root, width=60, height=30); txt_log.pack()
 
